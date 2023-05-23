37cd95de1e9f58a39a21430332b3993033ae6501
Juan Ballén <91291820+JCBallen@users.noreply.github.com>
2023-05-23 15:18:27 -0500

    url

--- src/public/test.js
+++ src/public/test.js
@@ -1,8 +1,9 @@
+import url from "./url.js";
 
 async function traerVacantes() {
-    const response = await fetch(`http://localhost:3000/consultarVacantes`, { method: "GET" });
+    const response = await fetch(`${url}/consultarVacantes`, { method: "GET" });
     const result = await response.json();
-    console.log(result[13]);
+    console.log(result);
 
 }
 
---
+++ src/public/url.js
@@ -0,0 +1,4 @@
+
+export const url= 'http://localhost:3000';
+
+export default url;
