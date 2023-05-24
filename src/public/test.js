c21570913ce4ecac6cf6668a22683865badc8b73
JulieIb03 <est.julie.ibarra@unimilitar.edu.co>
2023-05-23 20:11:44 -0500

    jiji

--- src/public/test.js
+++ src/public/test.js
@@ -3,77 +3,82 @@
 // let result;
 
 async function traerVacantes() {
-  const response = await fetch("https://agencia-empleo.azurewebsites.net/consultarVacantes", {
-    method: "GET",
-  });
+  const response = await fetch(
+    "http://localhost:3000/consultarVacantes",
+    {
+      method: "GET",
+    }
+  );
   const result = await response.json();
 
   // return result
+  setTimeout(() => {
+    const dataContainer = document.getElementById("vacantes");
 
-  const dataContainer = document.getElementById("vacantes");
+    result.forEach(function (item) {
+      console.log(item.cargo);
+      const div1 = document.createElement("div");
+      const div2 = document.createElement("div");
+      const div3 = document.createElement("div");
+      const div4 = document.createElement("div");
+      const div5 = document.createElement("div");
+      const div6 = document.createElement("div");
+      const div7 = document.createElement("div");
+      const div8 = document.createElement("div");
+      const cargo = item.cargo;
+      const pregrado = item.pregradorequerido
+        ? '<h5 class="degree-yes">Pregrado Requerido</h5>'
+        : '<h5 class="degree-no">Pregrado NO Requerido</h5>';
 
-  result.forEach(function (item) {
-    console.log(item.cargo);
-    const div1 = document.createElement("div");
-    const div2 = document.createElement("div");
-    const div3 = document.createElement("div");
-    const div4 = document.createElement("div");
-    const div5 = document.createElement("div");
-    const div6 = document.createElement("div");
-    const div7 = document.createElement("div");
-    const div8 = document.createElement("div");
-    const cargo = item.cargo;
-    const pregrado = item.pregradorequerido?'<h5 class="degree-yes">Pregrado Requerido</h5>':'<h5 class="degree-no">Pregrado NO Requerido</h5>';
+      div1.classList.add("col-md-6", "col-lg-4");
+      div2.classList.add("recent-job-item");
+      div3.classList.add("company-info");
+      div4.classList.add("logo");
+      div5.classList.add("content");
+      div6.classList.add("main-content");
+      div7.classList.add("recent-job-info");
+      div8.classList.add("salary");
 
-    div1.classList.add("col-md-6", "col-lg-4");
-    div2.classList.add("recent-job-item");
-    div3.classList.add("company-info");
-    div4.classList.add("logo");
-    div5.classList.add("content");
-    div6.classList.add("main-content");
-    div7.classList.add("recent-job-info");
-    div8.classList.add("salary");
-
-
-    div4.innerHTML =
-      '<a href="company-details.html"><img src="assets/img/companies/1.jpg" width="75" height="75 alt="Image-HasTech"></a>';
-    div5.innerHTML =
-      '<h4 class="name"><a href="/empresa">' +
-      item.nit +
-      '</a></h4><p class="address"><i class="bx bxs-map"></i>' +
-      item.idubicacion +
-      "</p>";
-    /* div6.innerHTML =
+      div4.innerHTML =
+        '<a href="company-details.html"><img src="assets/img/companies/1.jpg" width="75" height="75 alt="Image-HasTech"></a>';
+      div5.innerHTML =
+        '<h4 class="name"><a href="/empresa">' +
+        item.nombreempresa +
+        '</a></h4><p class="address"><i class="bx bxs-map"></i>' +
+        item.idubicacion +
+        "</p>";
+      /* div6.innerHTML =
       ; */
-    //div6.innerHTML = ;
-    div6.innerHTML =
-      //cargo.textContent +
-      '<h3 class="title"><a href="#">' +
-      cargo +
-      '</a></h3>' +
-      pregrado +
-      '<p class="desc">Fecha Inicio:' +
-      item.fechainicio +
-      "<br> Fecha Cierre:" +
-      item.fechacierre +
-      "<br> Rando Edad:" +
-      item.rango +
-      "</p>";
-    div8.innerHTML =
-      "<h4>" +
-      item.salario +
-      '</h4><br><a class="btn-theme btn-sm" href="">Postularse</a>';
+      //div6.innerHTML = ;
+      div6.innerHTML =
+        //cargo.textContent +
+        '<h3 class="title"><a href="#">' +
+        cargo +
+        "</a></h3>" +
+        pregrado +
+        '<p class="desc">Fecha Inicio:' +
+        item.fechainicio +
+        "<br> Fecha Cierre:" +
+        item.fechacierre +
+        "<br> Rando Edad:" +
+        item.rango +
+        "</p>";
+      div8.innerHTML =
+        "<h4>" +
+        item.salario +
+        '</h4><br><a class="btn-theme btn-sm" href="">Postularse</a>';
 
-    div1.appendChild(div2);
-    div2.appendChild(div3);
-    div2.appendChild(div6);
-    div2.appendChild(div7);
-    div3.appendChild(div4);
-    div3.appendChild(div5);
-    div7.appendChild(div8);
+      div1.appendChild(div2);
+      div2.appendChild(div3);
+      div2.appendChild(div6);
+      div2.appendChild(div7);
+      div3.appendChild(div4);
+      div3.appendChild(div5);
+      div7.appendChild(div8);
 
-    dataContainer.appendChild(div1);
-  });
+      dataContainer.appendChild(div1);
+    });
+  }, 500);
 }
 
 traerVacantes();
